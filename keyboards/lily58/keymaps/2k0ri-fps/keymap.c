e75cec99537f747a7d737c3c51699fb3e5d14573
2k0ri <esc13245@gmail.com>
2021-01-27 05:27:51 +0900

    add 2k0ri-fps

--- keyboards/lily58/keymaps/2k0ri-fps/keymap.c
+++ keyboards/lily58/keymaps/2k0ri-fps/keymap.c
@@ -35,25 +35,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* QWERTY
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * | ESC  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  =   |
+ * | ESC  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  ]   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | Tab  |'/RISE|   ,  |   .  |   P  |   Y  |                    |   F  |   G  |   C  |   R  |   L  |  /   |
+ * | Tab  |Q/RISE|   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  [   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |LCTRL |   A  |   O  |   E  |   U  |   I  |-------.    ,-------|   D  |   H  |   T  |   N  |S/RISE|  -   |
+ * |LCTRL |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |;/RISE|  '   |
  * |------+------+------+------+------+------|   `   |    |   \   |------+------+------+------+------+------|
- * |LShift|;/LOWR|   Q  |   J  |   K  |   X  |-------|    |-------|   B  |   M  |   W  |   V  |Z/LOWR|RShift|
+ * |LShift|Z/LOWR|   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |//LOWR|RShift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   |[/LCTL| LALT | LGUI | / Space /       \Enter \  | RGUI |[/RALT|   ]  |
+ *                   |-/LCTL| LALT | LGUI | / Space /       \Enter \  | RGUI |-/RALT|   =  |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
 
  [_QWERTY] = LAYOUT( \
-  KC_ESC,   KC_1,   KC_2,     KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL, \
-  KC_TAB,   LT(RAISE,KC_QUOT),KC_COMM, KC_DOT,  KC_P,    KC_Y,                     KC_F,    KC_G,    KC_C,    KC_R,    KC_L,           KC_SLSH, \
-  KC_LCTRL, KC_A,    KC_O,    KC_E,    KC_U,    KC_I,                     KC_D,    KC_H,    KC_T,    KC_N,    LT(RAISE, KC_S),KC_MINS, \
-  KC_LSFT,  LT(LOWER,KC_SCLN),KC_Q,    KC_J,    KC_K,    KC_X,    KC_GRV, KC_BSLS, KC_B,    KC_M,    KC_W,    KC_V,    LT(LOWER, KC_Z),KC_RSFT, \
-                  LCTL_T(KC_LBRC),KC_LALT,KC_LGUI,KC_SPC,KC_ENT,  KC_RGUI,RALT_T(KC_LBRC),KC_RBRC \
+  KC_ESC,   KC_1,          KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_RBRC, \
+  KC_TAB,   LT(RAISE,KC_Q),KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, \
+  KC_LCTRL, KC_A,          KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    LT(RAISE, KC_SCLN), KC_QUOT, \
+  KC_LSFT,  LT(LOWER,KC_Z),KC_X,    KC_C,    KC_V,    KC_B,    KC_GRV, KC_BSLS, KC_N,    KC_M,    KC_COMM, KC_DOT,  LT(LOWER, KC_SLSH), KC_RSFT, \
+                  LCTL_T(KC_MINS),KC_LALT,KC_LGUI,KC_SPC,KC_ENT,  KC_RGUI,RALT_T(KC_MINS),KC_EQL \
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
